1bf8d2108802b49d7392acb57943534605c4d9f4
HaoJ <110011336+Jethaoo@users.noreply.github.com>
2024-04-16 14:46:02 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cp44mcqeqs55.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "angjethao-employee"
+customregion = "us-east-1"
+
